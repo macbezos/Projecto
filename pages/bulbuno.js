e0e04572e310d4d8b05de76919470bc1c20c000f
macbezos <makdagascos67@gmail.com>
2021-11-03 13:16:03 +0300

    change 1 bulbuno

--- pages/bulbuno.js
+++ pages/bulbuno.js
@@ -3,5 +3,16 @@ import Head from 'next/head'
 export default function bulbuno() {
   return (
   <>
-  <h1><u>Bulb uno</u></h1><p>The oldest model of Bulb the bulb uno.</p></>
+  <h1 class="heading"><u>Bulb uno</u></h1><p>The oldest model of Bulb the bulb uno.</p>
+ <style jsx>{`
+   .heading {
+     text-align:center;
+     background-color:blue;
+   }
+    
+   
+   
+   
+  `}</style> 
+  </>
   )}
