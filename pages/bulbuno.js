3ee9e8955d8a4bd9b15a4ed295e0fb9df85cf41b
macbezos <92293298+macbezos@users.noreply.github.com>
2021-11-03 13:29:18 +0300

    Update bulbuno.js

--- pages/bulbuno.js
+++ pages/bulbuno.js
@@ -1,18 +1,18 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function bulbuno() {
   return (
   <>
-  <h1 class="heading"><u>Bulb uno</u></h1><p>The oldest model of Bulb the bulb uno.</p>
+  <h1 className="heading"><u>Bulb uno</u></h1><p>The oldest model of Bulb the bulb uno.</p>
+    <link>
+    <a href="/"><bold>HOME</bold></a>
+    </link>
  <style jsx>{`
    .heading {
-     text-align:center;
-     background-color:blue;
+     text-align: center;
+     background-color: blue;
    }
-    
-   
-   
-   
   `}</style> 
   </>
   )}
