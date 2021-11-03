a15fc6f84210e903edc55aa9f42b0f12f59ef8db
macbezos <makdagascos67@gmail.com>
2021-11-03 11:52:43 +0300

    bulb one

--- pages/index.js
+++ pages/index.js
@@ -4,45 +4,46 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Bulb Stores</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          We sell high quality Bulbs.
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Our bulbs are high class.
         </p>
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Bulb type Uno &rarr;</h3>
+            <p>Explore the features of bulb type uno.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Bulb type dos</h3>
+            <p>Explore the features of bulb type dos and its advantages.</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className="card"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Bulb type tres</h3>
+            <p>Discover the advantages and features of Bulb type tres.
+            </p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>The wonders of Excit Bulb</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Explore the features and advantages of Excit Bulb.
             </p>
           </a>
         </div>
